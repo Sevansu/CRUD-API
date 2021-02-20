1bec1effa92cd7babe2588cd59353e5db752824a
Sevansu <sevansupatel2122@gmail.com>
2021-02-20 17:23:16 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:9999"
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to CRUD application." });
+});
+
+
+const PORT = process.env.PORT || 9999;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
